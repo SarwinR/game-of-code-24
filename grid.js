7b08000e0c34a58087a08ab407f0b20c121e7b44
RFeiz <zief002@gmail.com>
2024-06-20 22:10:50 +0400

    Update grid.js

--- grid.js
+++ grid.js
@@ -24,7 +24,7 @@ function gameOver() {
 
 function gameWin() {
   displayMessage("system", "Congratulations! You have completed the level!");
-  if (currentLevel <= 1) {
+  if (currentLevel < 1) {
     loadLevel("./levels/javascript_lesson/config.json", currentLevel);
     disableChat();
     setupPlayer();
